ee1da4b1f8a3c9e327e779568574475146e93e56
Carlos Martorini <c7.silveira@gmail.com>
2021-09-20 09:58:47 -0300

    feat: Created functions on controllers to list all, list by id, delete, update and create Fast Food

--- app/controllers/fast_food_controller.py
+++ app/controllers/fast_food_controller.py
@@ -1,4 +1,52 @@
 from flask import request, jsonify
 from app.models.fast_food_model import FastFood
+from app.exceptions.fast_food_exceptions import FastFoodNotFoundError
+from psycopg2.errors import UniqueViolation
 
 
+def list_all_fast_food():
+    fast_food_list = FastFood.get_all()
+    
+    return jsonify(fast_food_list), 200
+
+
+def get_fast_food_by_id(id: int):
+    fast_food = FastFood.get_by_id(id)
+    
+    return fast_food, 200
+
+
+def delete_fats_food(id: int):
+    try:
+        delete_fast_food = FastFood.delete(id)
+
+        return delete_fats_food, 200
+    
+    except FastFoodNotFoundError as e:
+        return {'message': str(e)}, 404
+
+
+def update_fast_food(id: int):
+        try:
+            data = request.json
+
+            update_fast_food = FastFood.update(id, data)
+
+            return update_fast_food, 200
+        
+        except FastFoodNotFoundError as e:
+            return {'message': str(e)}, 404
+
+
+def create_fast_food():
+    data = request.json
+
+    try:
+        fast_food = FastFood(data)
+
+        new_fast_food = fast_food.save()
+
+        return new_fast_food, 201
+    
+    except UniqueViolation as e:
+        return {'message': str(e).split('\n')[-2]}, 409
